08ed951e5972b24f5e078050f0c1dcad36f50dee
Namu <leevan.david@pit.fr>
2023-11-22 16:58:26 +0100

    Changed Entity + Fixed Heritage

--- src/main/java/modele/Creneau.java
+++ src/main/java/modele/Creneau.java
@@ -1,12 +1,10 @@
 package modele;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.sql.Timestamp;
 
+@Entity
 public class Creneau {
 
     @Id
