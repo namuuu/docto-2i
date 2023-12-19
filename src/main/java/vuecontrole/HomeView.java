fc6d483b5724362a755ec66e3bed8fc5ec106729
TRETomas <tomas.treny@ig2i.centralelille.fr>
2023-12-19 14:17:34 +0100

    Interface login & planning

--- src/main/java/vuecontrole/HomeView.java
+++ src/main/java/vuecontrole/HomeView.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class HomeView extends JFrame {
     private JTabbedPane tabbedPane1;
@@ -12,11 +16,19 @@ public class HomeView extends JFrame {
     private JLabel labelNameApp;
     private JLabel labelDoctorName;
     private JButton logoutButton;
+    private JTable tablePlanningDocteur;
+    private JTable tablePlanningSalle;
+    private JLabel LabelDate;
+    private final String date;
 
     public HomeView() {
+        this.date = LocalDate.now().format(DateTimeFormatter.ofPattern("EEEE dd MMMM", Locale.FRANCE));
+        this.LabelDate.setText(date);
         setContentPane(panelPlanning);
         InitialisationFenetre();
         InitialisationTableauPlanningGlobal();
+        InitialisationTableauPlanningDocteur();
+        InitialisationTableauPlanningSalle();
 
         logoutButton.addActionListener(new ActionListener() {
             @Override
@@ -33,13 +45,43 @@ public class HomeView extends JFrame {
         DefaultTableModel model = new DefaultTableModel();
         model.addColumn("Heure");
         model.addColumn("Jour");
-        model.addRow(new Object[]{"Heure", "Lundi"});
+        model.addRow(new Object[]{"Heure", date});
         for(int i = 8; i < 22; i++) {
             model.addRow(new Object[]{i + "h00", "Rendez-vous n°" + i + " - Patient n°" + i + " - Salle n°" + i});
         }
         tablePlanningGlobal.setModel(model);
         tablePlanningGlobal.getColumnModel().getColumn(0).setPreferredWidth(150);
         tablePlanningGlobal.getColumnModel().getColumn(1).setPreferredWidth(750);
+    }
+
+    private void InitialisationTableauPlanningSalle(){
+        tablePlanningSalle.setEnabled(false);
+        tablePlanningSalle.setRowHeight(50);
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Heure");
+        model.addColumn("Jour");
+        model.addRow(new Object[]{"Heure", date});
+        for(int i = 8; i < 22; i++) {
+            model.addRow(new Object[]{i + "h00", "Rendez-vous n°" + i + " - Patient n°" + i + " - Salle n°" + i});
+        }
+        tablePlanningSalle.setModel(model);
+        tablePlanningSalle.getColumnModel().getColumn(0).setPreferredWidth(150);
+        tablePlanningSalle.getColumnModel().getColumn(1).setPreferredWidth(750);
+    }
+
+    private void InitialisationTableauPlanningDocteur(){
+        tablePlanningDocteur.setEnabled(false);
+        tablePlanningDocteur.setRowHeight(50);
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Heure");
+        model.addColumn("Jour");
+        model.addRow(new Object[]{"Heure", date});
+        for(int i = 8; i < 22; i++) {
+            model.addRow(new Object[]{i + "h00", "Rendez-vous n°" + i + " - Patient n°" + i + " - Salle n°" + i});
+        }
+        tablePlanningDocteur.setModel(model);
+        tablePlanningDocteur.getColumnModel().getColumn(0).setPreferredWidth(150);
+        tablePlanningDocteur.getColumnModel().getColumn(1).setPreferredWidth(750);
     }
 
     private void InitialisationFenetre() {
